aa3330a0080263eed9b27819b1666abad871ba4d
DEVELOPER\alqui <contacto@mamglez.com>
2024-07-03 16:06:29 +0200

    154 metodo personalizado para cerrar sesion

--- src/main/java/com/mamglez/controller/HomeController.java
+++ src/main/java/com/mamglez/controller/HomeController.java
@@ -6,6 +6,7 @@ import java.util.Collection;
 import java.util.Date;
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +18,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.WebDataBinder;
@@ -59,6 +61,13 @@ public class HomeController {
 	@GetMapping("/login")
 	public String mostrarLogin() {
 		return "formLogin";
+	}
+	
+	@GetMapping("/logout")
+	public String logout(HttpServletRequest request) {
+		SecurityContextLogoutHandler logoutHandler = new SecurityContextLogoutHandler();
+		logoutHandler.logout(request, null, null);
+		return "redirect:/login";
 	}
 	
 	@GetMapping("/index")
