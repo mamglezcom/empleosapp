fa7975c7809c735304ff606d6ce7f798e990cbb6
DEVELOPER\alqui <contacto@mamglez.com>
2024-07-03 19:22:34 +0200

    161 configurar databinding del formulario html con un objeto de modelo solicitud

--- src/main/java/com/mamglez/controller/SolicitudesController.java
+++ src/main/java/com/mamglez/controller/SolicitudesController.java
@@ -5,8 +5,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.mamglez.model.Solicitud;
 import com.mamglez.model.Vacante;
 import com.mamglez.service.IVacantesService;
 
@@ -18,11 +20,17 @@ public class SolicitudesController {
 	private IVacantesService serviceVacantes;
 	
 	@GetMapping("/create/{idVacante}")
-	public String crear(@PathVariable("idVacante") Integer idVacante, Model model) {
+	public String crear(Solicitud solicitud, @PathVariable("idVacante") Integer idVacante, Model model) {
 		Vacante vacante = serviceVacantes.buscarPorId(idVacante);
 		model.addAttribute("vacante", vacante);
 		System.out.println("idVacante: " + idVacante);
 		return "solicitudes/formSolicitud";
+	}
+	
+	@PostMapping("/save")
+	public String guardar(Solicitud solicitud) {
+		System.out.println(solicitud);
+		return "redirect:/";
 	}
 
 }
