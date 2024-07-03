0201200f6ded2dec6eab93b7c80fc3e471452571
DEVELOPER\alqui <contacto@mamglez.com>
2024-07-03 18:53:04 +0200

    159 configurar vista del formulario para enviar una solicitud de vacante

--- src/main/java/com/mamglez/controller/SolicitudesController.java
+++ src/main/java/com/mamglez/controller/SolicitudesController.java
@@ -1,17 +1,26 @@
 package com.mamglez.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.mamglez.model.Vacante;
+import com.mamglez.service.IVacantesService;
+
 @Controller
 @RequestMapping("/solicitudes")
 public class SolicitudesController {
 	
+	@Autowired
+	private IVacantesService serviceVacantes;
+	
 	@GetMapping("/create/{idVacante}")
 	public String crear(@PathVariable("idVacante") Integer idVacante, Model model) {
+		Vacante vacante = serviceVacantes.buscarPorId(idVacante);
+		model.addAttribute("vacante", vacante);
 		System.out.println("idVacante: " + idVacante);
 		return "solicitudes/formSolicitud";
 	}
